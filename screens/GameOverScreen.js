5a31609972304b7dfb7d973140287ad0da702b10
SBhattacharya45 <umyadeepb27@gmail.com>
2020-10-05 15:13:44 +0530

    gameover orientation changes

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding: 10
+        paddingVertical: 10
     },
     image: {
         width: '100%',
